41a393c60262d986b1b18f5f378a1e99cdd0e98a
henryzord <henry.cagnini@gmail.com>
2024-01-31 20:29:25 -0300

    update dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='db2',
@@ -6,7 +10,8 @@ setup(
     url='https://github.com/COPLIN-UFSM/db2',
     author='Henry Cagnini',
     author_email='henry.cagnini@ufsm.br',
+    long_description=long_description,
     py_modules=['db2'],
-    install_requires=['ibm_db'],
-    python_requires='>=3.7',
+    install_requires=['ibm_db==3.1.4'],
+    python_requires='==3.11.*',
 )
