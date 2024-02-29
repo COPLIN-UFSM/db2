2e6d41fbc9243083f98a64ead263b285a476402a
henryzord <henry.cagnini@gmail.com>
2024-02-29 07:36:31 -0300

    minor fix to setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf-8') as r
 
 setup(
     name='coplin_db2',
-    version='1.4.3.1',
+    version='1.4.3.2',
     url='https://github.com/COPLIN-UFSM/db2',
     author='Henry Cagnini',
     author_email='henry.cagnini@ufsm.br',
     description='Um módulo de conveniência para manipulação de bancos de dados IBM DB2 em Python.',
     long_description_content_type='text/markdown',
     long_description=long_description,
-    packages=['db2'],
+    packages=['db2', 'db2.utils'],
     py_modules=['db2'],
     install_requires=['ibm_db==3.1.4', 'numpy', 'pandas'],
     python_requires='>=3.8,<3.12'
